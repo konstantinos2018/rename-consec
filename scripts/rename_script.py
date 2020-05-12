4b65714f078f87e40276df7aada001d0343d3dc9
konstantinos2018 <kvlachos.geo@gmail.com>
2020-05-12 14:57:57 +0300

    Change cdir of files

--- scripts/rename_script.py
+++ scripts/rename_script.py
@@ -4,7 +4,7 @@
 # =============================================================================
 import os
 
-cdir = 'H:\Bells Palsy project\clips_and_photos'
+cdir = 'H:\Bells Palsy project\clips_and_photos\Backup'
 fnames = os.listdir(cdir)
 fextens = {'Image': ['jpg', 'jpeg'],
           'Video': ['mp4']

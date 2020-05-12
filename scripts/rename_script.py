49184bc613fcfd2e19ef8f2247fb229e91f2d95f
konstantinos2018 <kvlachos.geo@gmail.com>
2020-05-12 13:59:30 +0300

    Read filenames, import and add file extensions

--- scripts/rename_script.py
+++ scripts/rename_script.py
@@ -1,2 +1,11 @@
 # -*- coding: utf-8 -*-
+# =============================================================================
+# Imports
+# =============================================================================
+import os
 
+cdir = 'H:\Bells Palsy project\clips_and_photos'
+fnames = os.listdir(cdir)
+fextens = {'Image': ['jpg', 'jpeg'],
+          'Video': ['mp4']
+          }

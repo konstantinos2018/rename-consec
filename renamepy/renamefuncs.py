0ad1c6e442f14ea679f710122d13ec5dac9b373f
konstantinos2018 <kvlachos.geo@gmail.com>
2020-05-12 15:03:47 +0300

    Add function that extracts file extension

--- renamepy/renamefuncs.py
+++ renamepy/renamefuncs.py
@@ -7,3 +7,16 @@ Created on Tue May 12 13:40:25 2020
 @linkedin: www.linkedin.com/in/kostasvlachosgrs
 """
 
+def extract_extension(fname):
+    """Extract the extension of a given file
+    
+    Args:
+        fname = string of the filename
+        
+    Returns:
+        extension = string of the extension
+    """
+    temp = fname.split('.')
+    extension = temp[-1]  # last element of list (i.e. extension)
+    
+    return extension
